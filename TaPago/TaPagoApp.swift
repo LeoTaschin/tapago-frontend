6ed6136df14af3908d92fd568f9beb65237864d2
Leonardo Taschin <leoctaschin@gmail.com>
2025-02-04 14:16:32 -0300

    Initial Commit

---
+++ TaPago/TaPagoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaPagoApp.swift
+//  TaPago
+//
+//  Created by Leonardo Taschin on 04/02/25.
+//
+
+import SwiftUI
+
+@main
+struct TaPagoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
