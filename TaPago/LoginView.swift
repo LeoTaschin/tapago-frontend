ed40bf882bd6780f3f7d297c9237c36a7e5c3a8b
Leonardo Taschin <leoctaschin@gmail.com>
2025-02-04 15:20:19 -0300

    Implementacao tela de login

---
+++ TaPago/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  TaPago
+//
+//  Created by Leonardo Taschin on 04/02/25.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
