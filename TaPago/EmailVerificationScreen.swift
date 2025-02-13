011b793fb86bc9206c62736fc0de4dbda052cb3f
Leonardo Taschin <leoctaschin@gmail.com>
2025-02-13 09:50:39 -0300

    Envio de email para verificacao

---
+++ TaPago/EmailVerificationScreen.swift
@@ -0,0 +1,18 @@
+//
+//  EmailVerificationScreen.swift
+//  TaPago
+//
+//  Created by Leonardo Taschin on 13/02/25.
+//
+
+import SwiftUI
+
+struct EmailVerificationScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmailVerificationScreen()
+}
